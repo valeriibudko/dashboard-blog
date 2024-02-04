e8d31c2ca7594d705ff012fda818c1074b26c7ef
vbudko <valerij.budko@gmail.com>
2024-02-04 23:13:52 +0000

    header base

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import { PureComponent } from 'react'
 import { Navbar, TextInput, Button } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa';
 
 
-export default class Header extends PureComponent {
-  render() {
+// export default class Header extends PureComponent {
+
+//   render() {
+
+export default function Header() {
+
+    const path = useLocation().pathname;
+
     return (
       <Navbar className='border-b-2'>
         <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -18,8 +24,30 @@ export default class Header extends PureComponent {
                 placeholder='Search...'
                 rightIcon={AiOutlineSearch}
                 className='hidden lg:inline'
-            />
+                />
         </form>
+        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+            <AiOutlineSearch />
+        </Button>
+
+        <Navbar.Collapse>
+                <Navbar.Link active={path === "/"} as={'div'}>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/about"} as={'div'}>
+                    <Link to='/about'>
+                        About
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/projects"} as={'div'}>
+                    <Link to='/projects'>
+                        Projects
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+
         <Button className='w-12 h-10 hidden lg:hidden' color='gray' pill>
             <AiOutlineSearch />
         </Button>
@@ -30,28 +58,12 @@ export default class Header extends PureComponent {
             <Link to='/sign-in'>
             <Button gradientDuoTone='purpleToBlue'>
                 Sign In
-</Button>
+                </Button>
             </Link>
-
-            <Navbar.Collapse>
-                <Navbar.Link>
-                    <Link to='/'>
-                        Home
-                    </Link>
-                </Navbar.Link>
-                <Navbar.Link>
-                    <Link to='/about'>
-                        About
-                    </Link>
-                </Navbar.Link>
-                <Navbar.Link>
-                    <Link to='/projects'>
-                        Projects
-                    </Link>
-                </Navbar.Link>
-            </Navbar.Collapse>
+            <Navbar.Toggle />
         </div>
+
       </Navbar>
     )
-  }
+//   }
 }
