c37db5745dee383863f9caede8317799371f0cb1
vbudko <valerij.budko@gmail.com>
2024-02-05 22:58:33 +0000

    MongoDB implement

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const prefixConsole = '=> ';
+
+dotenv.config();
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+        console.log(prefixConsole + 'MongoDB is connected')
+    })
+.catch((err) => {
+        console.error(prefixConsole + err)
+    })
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(prefixConsole + 'Server is running on port ' + port)
+});
