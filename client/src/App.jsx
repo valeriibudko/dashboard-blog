c00c6e0ef6c41e1b66454bbaf18b63a4501050bc
vbudko <valerij.budko@gmail.com>
2024-03-08 13:54:09 +0000

    Make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
-
+import PrivateRoute from './components/PrivateRoute'
 
 
 export default function App() {
@@ -19,7 +19,9 @@ export default function App() {
       <Route path='/about' element={<About />}/>
       <Route path='/sign-in' element={<SignIn />}/>
       <Route path='/sign-up' element={<SignUp />}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+      </Route>
       <Route path='/projects' element={<Projects />}/>
     </Routes>
     <FooterCont/>
--- client/src/redux/theme/themeSlice.js
+++ client/src/redux/theme/themeSlice.js
@@ -8,7 +8,7 @@ const themeSlice = createSlice({
     name: 'theme',
     initialState,
     reducers: {
-        toggleTheme: (state) => {
+        toggleTheme : (state) => {
             state.theme = state.theme === 'light' ? 'dark' : 'light'
         },
     },
