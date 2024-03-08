251f812e61c4ada7b0d755cf47dd69558a7b9f5c
vbudko <valerij.budko@gmail.com>
2024-03-08 17:48:53 +0000

    Profile page ui

--- client/src/components/dash/DashProfile.jsx
+++ client/src/components/dash/DashProfile.jsx
@@ -1,5 +1,31 @@
+
+import { TextInput, Button } from 'flowbite-react';
+import { useSelector } from 'react-redux';
+
 export default function DashProfile() { 
+    const { currentUser } = useSelector(state => state.user)
+
     return (
-        <div>DashProfile</div>
+        <div className='max-w-lg mx-auto p-3 w-full'>
+            <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+            <form className='flex flex-col gap-4'>
+                <div className='w-32 h-32 self-center cursor-pointer overflow-hidden rounded-full shadow-xl'>
+
+                   <img src={currentUser.profilePicture} alt="user"  
+                   className='rounded-full w-full h-full bottom-8 object-cover border-[#41ac65]'/>
+                </div>
+
+                <TextInput type='text' id='username' placeholder='username' 
+                defaultValue={currentUser.username}/>
+                <TextInput type='text' id='email' placeholder='email' 
+                defaultValue={currentUser.email}/>
+                <TextInput type='text' id='password' placeholder='password' />
+                <Button type='submit' gradientDuoTone={'greenToBlue'} outline>Update</Button>
+            </form>
+            <div className='text-red-500 flex justify-between mt-5'>
+                <span className='cursor-pointer'>Delete acount</span>
+                <span className='cursor-pointer'>Sign out</span>
+            </div>
+        </div>
     )
 }
--- client/src/components/dash/DashSidebar.jsx
+++ client/src/components/dash/DashSidebar.jsx
@@ -20,11 +20,15 @@ export default function DashSidebar() {
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Link to={'/dashboard?tab=profile'}>
-                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User serserserserserserser serserser"} labelColor='dark'>
+                    {/* <Link to='/dashboard?tab=profile'> */}
+                        <Sidebar.Item 
+                        active={tab === 'profile'} 
+                        icon={HiUser} 
+                        label={"User"} 
+                        labelColor='dark'>
                                 Profile
-                    </Sidebar.Item>
-                    </Link>
+                        </Sidebar.Item>
+                    {/* </Link> */}
                     <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
                         Sign out
                     </Sidebar.Item>
